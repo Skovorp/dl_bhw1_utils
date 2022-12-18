d03b57afba7024765bde8733b6944df520e44f21
Peter Skov <peter.skovorodnikov@gmail.com>
2022-12-18 07:21:03 +0300

    add early stopping

--- training_functions.py
+++ training_functions.py
@@ -1,4 +1,5 @@
 #!g1.1
+import time
 
 from torch import nn
 import torch
@@ -6,6 +7,7 @@ from tqdm import tqdm
 import numpy as np
 import wandb
 import pandas as pd
+import time
 
 
 def rand_bbox(W, H, lmd):
@@ -76,7 +78,12 @@ def train_epoch_cutmix(model, optimizer, train_loader, alpha, aug_possibility, l
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     model.train()
-    for data, target in tqdm(train_loader):
+    time_buff_size = 10
+    too_long_for_batch = 0.25
+    time_buff = np.zeros(time_buff_size)
+
+    for i, (data, target) in tqdm(enumerate(train_loader)):
+        start_time = time.time()
         data, target = data.to(device), target.to(device)
 
         coinflip = np.random.rand()
@@ -105,6 +112,11 @@ def train_epoch_cutmix(model, optimizer, train_loader, alpha, aug_possibility, l
         else:
             acc = (pred.argmax(1) == target).sum().item()
         acc_sum += acc
+
+        end_time = time.time()
+        time_buff[i % time_buff_size] = end_time - start_time
+        if np.mean(time_buff) > too_long_for_batch:
+            raise Exception(f"Batch took to long. Avg of 5: {np.mean(time_buff)}")
 
     return loss_sum / len(train_loader.dataset), acc_sum / len(train_loader.dataset)
 
@@ -194,6 +206,13 @@ def train(model, optimizer, scheduler, n_epochs, train_loader, val_loader, alpha
             artifact = wandb.Artifact(f'model_checkpoint_{wandb_run.name}', type='model')
             artifact.add_file(f'/kaggle/working/model_checkpoint_{wandb_run.name}.pth')
             wandb_run.log_artifact(artifact)
+
+        if epoch == 5 and val_loss < 0.15:
+            print(f"Accuracy on 5th epoch was too low: {val_loss}")
+            return best_val_acc
+        if epoch == 10 and val_loss < 0.30:
+            print(f"Accuracy on 10th epoch was too low: {val_loss}")
+            return best_val_acc
     return best_val_acc
 
 def predict_test(model, loader):
